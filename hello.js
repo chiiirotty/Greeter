1e1d2ad7d15e7745c49aec9537d90198b3bffabf
小島 千尋 <kojima_chihiro@comture.com>
2020-10-16 15:24:20 +0900

    second commit

--- hello.js
+++ hello.js
@@ -1,11 +1,87 @@
-function greeter(person) {
-    return "こんにちは、 " + person.lastName + " " + person.firstName + "さん！よろしくね^^";
+var display;
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Object.defineProperty(Person.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        set: function (name) {
+            this._name = name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Person;
+}());
+var person = new Person();
+function validation(errorFlg) {
+    var message;
+    if (errorFlg == 0) {
+        message = '空欄だよ。';
+    }
+    else if (errorFlg == 1) {
+        message = '未来の日付だよ。';
+    }
+    else {
+        message = '不明なエラーだよ。';
+    }
+    return message;
 }
-function onButtonClick() {
-    var lastName = document.getElementById("lastname");
-    var myouji = lastName.value;
-    var firstName = document.getElementById("firstname");
-    var namae = firstName.value;
-    var user = { lastName: myouji, firstName: namae };
-    document.body.innerHTML = greeter(user);
+function regName() {
+    var Name = document.getElementById("name");
+    var name = Name.value;
+    person.name = name;
+    if (name == "") {
+        display = validation(0);
+        window.alert(display);
+    }
+    else {
+        display = greeter(person.name);
+        document.body.innerHTML = display;
+    }
+}
+function regAge() {
+    var Birthday = document.getElementById("birthday");
+    var birthday = Birthday.value;
+    var birthDate = new Date(birthday);
+    var today = new Date();
+    if (birthday == "") {
+        display = validation(0);
+        window.alert(display);
+    }
+    else if (birthDate.getTime() > today.getTime()) {
+        display = validation(1);
+        window.alert(display);
+    }
+    else {
+        var birthdayList = splitBirthday(birthday);
+        var birthYear = parseInt(birthdayList[0]);
+        var birthMonth = parseInt(birthdayList[1]);
+        var birthDay = parseInt(birthdayList[2]);
+        display = tellPastEvents(birthYear, birthMonth, birthDay);
+        document.body.innerHTML = display;
+    }
+}
+function greeter(name) {
+    var greet = '<p>こんにちは、 ' + name + 'さん！よろしくね^^</p>';
+    var askBirthday = '<p>生年月日を教えて！</p>';
+    var startForm = '<form name="form2" id="id_form2" action="">';
+    var text = '<input type="date" id="birthday">';
+    var button = '<input type="button" value="決定" onclick="regAge();" />';
+    var endForm = '</form>';
+    var back = '<p><a href="index.html">もどる</a></p>';
+    return greet + askBirthday + startForm + text + button + endForm + back;
+}
+function splitBirthday(birthday) {
+    var birthdayList = birthday.split('-');
+    return birthdayList;
+}
+function tellPastEvents(birthYear, birthMonth, birthDay) {
+    var birthday = birthYear + '年' + birthMonth + '月' + birthDay + '日';
+    var showWikipedia = '<a href="https://ja.wikipedia.org/wiki/' + birthYear + '%E5%B9%B4%E3%81%AE%E6%97%A5%E6%9C%AC#' + birthMonth + '%E6%9C%88" target="blank">こんな</a>';
+    var reply = '<p>' + person.name + 'さんは' + birthday + 'に生まれたんだね！</p>';
+    var tellEvent = '<p>' + person.name + 'さんの生まれた月には' + showWikipedia + 'ことがあったよ！</p>';
+    var back = '<p><a href="index.html">さいしょにもどる</a></p>';
+    return reply + tellEvent + back;
 }
--- hello.ts
+++ hello.ts
@@ -1,19 +1,85 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let display;
+
+class Person {
+    _name: string; 
+    get name(): string {
+        return this._name;
+    }
+    set name(name: string) {
+        this._name = name;
+    }
 }
 
-function greeter(person: Person) {
-    return "こんにちは、 " + person.lastName + " " + person.firstName + "さん！よろしくね^^";
+let person = new Person();
+
+function regName() {
+    let Name: HTMLInputElement  = <HTMLInputElement>document.getElementById("name");
+    let name: string = Name.value;
+    person.name = name;
+    if (name == "") {
+        display = validation(0);
+        window.alert(display);
+    } else {
+        display = greeter(person.name);
+        document.body.innerHTML = display;
+    }
 }
 
-function onButtonClick() {
-    let lastName: HTMLInputElement  = <HTMLInputElement>document.getElementById("lastname");
-    let myouji: string = lastName.value;
-    let firstName: HTMLInputElement  = <HTMLInputElement>document.getElementById("firstname");
-    let namae: string = firstName.value;
-    let user = { lastName: myouji, firstName: namae };
-    document.body.innerHTML = greeter(user);
+function regAge() {
+    let Birthday: HTMLInputElement  = <HTMLInputElement>document.getElementById("birthday");
+    let birthday: string = Birthday.value;
+    let birthDate = new Date(birthday);
+    let today = new Date();
+    if (birthday == "") {
+        display = validation(0);
+        window.alert(display);
+    } else if ( birthDate.getTime() > today.getTime()) {
+        display = validation(1);
+        window.alert(display);
+    } else {
+        let birthdayList = splitBirthday(birthday);
+        let birthYear = parseInt(birthdayList[0]);
+        let birthMonth = parseInt(birthdayList[1]);
+        let birthDay = parseInt(birthdayList[2]);
+        display = tellPastEvents(birthYear, birthMonth, birthDay);
+        document.body.innerHTML = display;
+    }
 }
 
+function greeter(name: string) {
+    let greet = '<p>こんにちは、 ' + name + 'さん！よろしくね^^</p>';
+    let askBirthday = '<p>生年月日を教えて！</p>';
+    let startForm = '<form name="form2" id="id_form2" action="">';
+    let text = '<input type="date" id="birthday">';
+    let button = '<input type="button" value="決定" onclick="regAge();" />';
+    let endForm = '</form>';
+    let back = '<p><a href="index.html">もどる</a></p>';
+    return greet + askBirthday + startForm + text + button + endForm + back;
+}
+
+function splitBirthday(birthday: string) {
+    let birthdayList = birthday.split('-');
+    return birthdayList;
+}
+
+function tellPastEvents(birthYear: number, birthMonth: number, birthDay: number) {
+    let birthday = birthYear + '年' + birthMonth + '月' + birthDay + '日';
+    let showWikipedia = '<a href="https://ja.wikipedia.org/wiki/' + birthYear + '%E5%B9%B4%E3%81%AE%E6%97%A5%E6%9C%AC#' + birthMonth + '%E6%9C%88" target="blank">こんな</a>';
+    let reply = '<p>'+ person.name + 'さんは、' + birthday + 'に生まれたんだね！</p>';
+    let tellEvent = '<p>' + person.name + 'さんの生まれた月には、' + showWikipedia + 'ことがあったよ！</p>';
+    let back = '<p><a href="index.html">さいしょにもどる</a></p>';
+    return reply + tellEvent + back;
+}
+
+function validation(errorFlg: number) {
+    let message;
+    if (errorFlg == 0) {
+        message = '空欄だよ。';
+    } else if (errorFlg == 1) {
+        message = '未来の日付だよ。';
+    } else {
+        message = '不明なエラーだよ。';
+    }
+    return message;
+}
 
